a787913c72aa843829ca88b56736aef20edd1055
mdismail1503 <mdismail1503@gmail.com>
2023-12-14 14:02:36 +0530

    form commit

--- controllers/viewController.js
+++ controllers/viewController.js
@@ -194,9 +194,7 @@ const createSendToken = (user, statusCode, res) => {
   const token = signToken(user._id);
   //console.log(token);
   const cookieOptions = {
-    expires: new Date(
-      Date.now() + process.env.JWT_COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000
-    ),
+    expires: new Date(Date.now() + 80 * 24 * 60 * 60 * 1000),
     httpOnly: true,
   };
   if (process.env.NODE_ENV === "production") cookieOptions.secure = true;
@@ -221,7 +219,6 @@ const createSendToken = (user, statusCode, res) => {
 
 exports.signupComplete = catchAsync(async (req, res, next) => {
   try {
-    console.log(req);
     const adminConfirmation = process.env.ADMIN_SECRET;
     if (
       req.body.secretForAdmin !== adminConfirmation &&
