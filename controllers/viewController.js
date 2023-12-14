39583a8d0844e1db56aa03916713bdc54446747f
mdismail1503 <mdismail1503@gmail.com>
2023-12-14 13:21:29 +0530

    body parser

--- controllers/viewController.js
+++ controllers/viewController.js
@@ -7,7 +7,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 
 const app = express();
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.urlencoded({ extended: true }));
 
 const Party = require("../models/partyModel");
 
